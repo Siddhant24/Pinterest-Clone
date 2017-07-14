0670cd81c21cddcb0db2b39451d7ea51c0ae38b0
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-15 01:13:42 +0530

    Update passport.js

--- app/config/passport.js
+++ app/config/passport.js
@@ -35,8 +35,6 @@ module.exports = function (passport) {
 					newUser.github.id = profile.id;
 					newUser.github.username = profile.username;
 					newUser.github.displayName = profile.displayName;
-					newUser.github.publicRepos = profile._json.public_repos;
-					newUser.nbrClicks.clicks = 0;
 
 					newUser.save(function (err) {
 						if (err) {
