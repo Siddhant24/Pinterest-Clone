68cbdf793c728e1fe2f6e6f3db8955c402a64470
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-16 19:38:35 +0000

    controller for getting user info

--- app/controllers/userController.client.js
+++ app/controllers/userController.client.js
@@ -4,7 +4,6 @@
 
    var profileId = document.querySelector('#profile-id') || null;
    var profileUsername = document.querySelector('#profile-username') || null;
-   var profileRepos = document.querySelector('#profile-repos') || null;
    var displayName = document.querySelector('#display-name');
    var apiUrl = appUrl + '/api/:id';
 
@@ -28,10 +27,5 @@
       if (profileUsername !== null) {
          updateHtmlElement(userObject, profileUsername, 'username');   
       }
-
-      if (profileRepos !== null) {
-         updateHtmlElement(userObject, profileRepos, 'publicRepos');   
-      }
-
    }));
 })();
