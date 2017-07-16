09a08505ffa0e064c4233f95dce59f4eb4d1e5b8
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-16 19:39:24 +0000

    routes for authentication and login

--- app/routes/index.js
+++ app/routes/index.js
@@ -13,7 +13,7 @@ module.exports = function (app, passport) {
 	}
 
 	app.route('/')
-		.get(function (req, res) {
+		.get(isLoggedIn, function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
@@ -35,14 +35,14 @@ module.exports = function (app, passport) {
 
 	app.route('/api/:id')
 		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
+			res.json(req.user.twitter);
 		});
 
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
+	app.route('/auth/twitter')
+		.get(passport.authenticate('twitter'));
 
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
+	app.route('/auth/twitter/callback')
+		.get(passport.authenticate('twitter', {
 			successRedirect: '/',
 			failureRedirect: '/login'
 		}));
