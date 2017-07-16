54ada9ccd653a3bcb6c7fbdc7485a8766004bebf
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-16 19:36:46 +0000

    added twitter OAuth strategy

--- app/config/auth.js
+++ app/config/auth.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-	'githubAuth': {
-		'clientID': process.env.GITHUB_KEY,
-		'clientSecret': process.env.GITHUB_SECRET,
-		'callbackURL': process.env.APP_URL + 'auth/github/callback'
+	'twitterAuth': {
+		'consumerKey': process.env.TWITTER_KEY,
+		'consumerSecret': process.env.TWITTER_SECRET,
+		'callbackURL': process.env.APP_URL + 'auth/twitter/callback'
 	}
 };
