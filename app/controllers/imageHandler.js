2d029f7081b7e5d83edb3177aec0f98e689cefd0
Siddhant Bhakar <sidbhakar@gmail.com>
2017-07-18 17:51:31 +0000

    added delete and like eventlisteners; added test for checking previous voters

--- app/controllers/imageHandler.js
+++ app/controllers/imageHandler.js
@@ -11,9 +11,9 @@ module.exports = {
         newImage.owner = user_id;
         newImage.link = data.link;
         newImage.caption = data.caption;
-        newImage.likes.push(user_id);
+        newImage.likes.push("596e47b370e9d54a0c0288c5");
         newImage.number = 0;
-        console.log(newImage);
+        //console.log(newImage);
         newImage.save(function(err) {
             if (err) {
                 throw err;
@@ -27,7 +27,7 @@ module.exports = {
                 owner: user_id
             }, function(err, docs) {
                 if (err) console.error(err);
-                console.log(docs);
+              //  console.log(docs);
                 resolve(docs);
             });
         });
@@ -51,22 +51,71 @@ module.exports = {
         });
     },
 
-    addLike: function(voter_id, image_id) {
-        Image.findOneAndUpdate({
-            _id: image_id
-        }, {
-            $push: {
-                likes: voter_id
-            },
-            $inc: {
-                number: 1
-            }
-        }, {
-            new: true
-        }, function(err, doc) {
-            if (err) console.log(err);
-            console.log(doc);
-        });
+    addLike: async function(voter_id, image_id) {
+        function hasLiked() {
+            return new Promise(function(resolve, reject) {
+                Image.find({
+                    _id: image_id
+                }, function(err, doc) {
+                    if (err) console.error(err);
+                    console.log(doc[0].likes);
+                    console.log(voter_id);
+                    doc[0].likes.every(function(id, index) {
+                        if (JSON.stringify(id) === JSON.stringify(voter_id) && doc[0].number !== 0) {
+                            console.log("resolved");
+                            reject("found");
+                            return false;
+                        }
+                        else if (index === doc[0].likes.length - 1) {
+                            console.log("rejected");
+                            resolve();
+                        }
+                        else {
+                            return true;
+                        }
+                    });
+                });
+            });
+        }
+        try {
+            await hasLiked();
+            Image.findOneAndUpdate({
+                _id: image_id
+            }, {
+                $push: {
+                    likes: voter_id
+                },
+                $inc: {
+                    number: 1
+                }
+            }, {
+                new: true
+            }, function(err, doc) {
+                if (err) console.log(err);
+                console.log(doc);
+            });
+            
+            return "added like";
+        }
+        catch (msg) {
+            console.log("catch..." + msg);
+            Image.findOneAndUpdate({
+                _id: image_id
+            }, {
+                $pull: {
+                    likes: voter_id
+                },
+                $inc: {
+                    number: -1
+                }
+            }, {
+                new: true
+            }, function(err, doc) {
+                if (err) console.log(err);
+                console.log(doc);
+            });
+            
+            return "deleted like";
+        }
     }
-
 };
--- app/controllers/onLoadController.myPics.js
+++ app/controllers/onLoadController.myPics.js
@@ -6,24 +6,59 @@
     function newGridElement(image) {
         var gridItem = document.createElement('div');
         gridItem.setAttribute("class", "grid-item text-center");
-        gridItem.innerHTML = '<i class="fa fa-times-circle-o" aria-hidden="true" style="font-size:25px;" id=\"' + image._id + '\"></i>';
+        gridItem.innerHTML = '<i class="fa fa-times-circle-o delete" aria-hidden="true" style="font-size:25px;" id=\"' + image._id + '\"></i>';
         gridItem.innerHTML += '<div class="grid-image"><img src=\"' + image.link + '\" class="img-fluid"></div>';
-        gridItem.innerHTML += '<i class="fa fa-heart" aria-hidden="true" style="font-size:25px;" id=\".' + image._id + '\"></i>';
+        gridItem.innerHTML += '<div class="caption-text text-center">' + image.caption + '</div>';
+        gridItem.innerHTML += '<i class="fa fa-heart like" aria-hidden="true" style="font-size:20px;" id=\".' + image._id + '\"></i>' + image.number;
         grid.append(gridItem);
     }
 
-    ajaxFunctions.ready(async function() {
-        function fetchData() {
-            return new Promise(function(resolve, reject) {
-                ajaxFunctions.ajaxRequest('GET', appUrl + '/myPics', function(data) {
-                    //console.log(data);
-                    resolve(JSON.parse(data));
-                });
+    function addImages(data) {
+        return new Promise(function(resolve, reject) {
+            data.forEach((image, index) => {
+                newGridElement(image);
+                if (index === data.length - 1) resolve();
             });
-        }
+        }).then(function() {
+            console.log("added");
+        });
+    }
 
-        var data = await fetchData();
-        data.forEach(image => newGridElement(image));
-        console.log(data);
+    function fetchData() {
+        return new Promise(function(resolve, reject) {
+            ajaxFunctions.ajaxRequest('GET', appUrl + '/myPics', function(data) {
+                resolve(JSON.parse(data));
+            });
+        });
+    }
+
+    ajaxFunctions.ready(async function() {
+        try {
+            var data = await fetchData();
+            await addImages(data.reverse());
+            console.log(document.querySelectorAll('.delete').forEach(val => console.log(val)));
+            document.querySelectorAll('.delete').forEach(val => val.addEventListener('click', function(e) {
+                console.log(e.target.id);
+                ajaxFunctions.ajaxPostRequest({
+                    image_id: e.target.id
+                }, appUrl + '/allPics', function(msg) {
+                    console.log(msg);
+                    if (msg === 'deleted') window.location.reload();
+                });
+            }));
+
+            document.querySelectorAll('.like').forEach(val => val.addEventListener('click', function(e) {
+                console.log(e.target.id.slice(1));
+                ajaxFunctions.ajaxPostRequest({
+                    image_id: e.target.id.slice(1)
+                }, appUrl + '/myPics', function(msg) {
+                    console.log(msg);
+                    if(msg === 'added like' || msg === 'deleted like') window.location.reload();
+                });
+            }));
+        }
+        catch (msg) {
+            console.log(msg);
+        }
     });
 })();
